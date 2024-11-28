956784ea0d502e830b5f1f73ffe0d39d0f955d40
奶喵 <75056749+Meow-OuO@users.noreply.github.com>
2024-11-29 04:51:06 +0800

    Update bingo.js

--- js/bingo.js
+++ js/bingo.js
@@ -1,17 +1,17 @@
 function rand(max) {
-  return Math.floor(Math.random() * max)+1;
+  return Math.floor(Math.random() * max) + 1;
 }
 
 function roll() {
-	let num = 80;
-	let arr = [];
-	let random = rand(num);
-	
-	for (i = 1; i <= 20; i++) {
-		arr.push(random);
-		while (arr.includes(random)) {
-			random = rand(num);
-		}
-	}
-	document.getElementById("bingo").innerHTML='開獎號碼:'+"&nbsp;"+arr.sort(function(a, b){return a-b});
+  let num = 80;
+  let arr = [];
+
+  while (arr.length < 20) {
+    let random = rand(num);
+    if (!arr.includes(random)) {
+      arr.push(random);
+    }
+  }
+
+  document.getElementById("bingo").innerHTML = "開獎號碼: " + "&nbsp;" + arr.sort(function (a, b) { return a - b; });
 }
